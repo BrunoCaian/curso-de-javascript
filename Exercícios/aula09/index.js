e7092455a8049a01077e2a8361952102bae2e0a9
BrunoCaian <brunocaian81@gmail.com>
2024-07-11 18:41:49 -0300

    Exercícios

--- Exercícios/aula09/index.js
+++ Exercícios/aula09/index.js
@@ -1,4 +1,16 @@
-// Diferença entre var e let: com var é possível declarar e redeclarar uma variável, já com let isso foi corrigido.
-var nome = 'Bruno'
-var nome = 'Caian'
-console.log(nome)
+// String, number, undefined, null, boolean, symbol
+const name = 'Bruno' // String
+const name1 = "Bruno" // String
+const name2 = `Bruno` // String
+const num1 = 10 // Number
+const num2 = 10.55 // Number
+let nomeAluno // Undefined -> não aponta pra local nenhum
+const sobrenomeAluno = null // Nulo -> não aponta pra lugar nenhum
+const aprovado = false // Boolean = true, false (lógico)
+
+let a = 2
+const b = a
+console.log(a, b) // 2, 2
+
+a = 3
+console.log(a, b) // 3, 2
---
+++ Exercícios/aula10/index.js
@@ -0,0 +1,15 @@
+/*
+  * Aritméticos
+    * + Adição / Concatenação
+    * - / *
+    * ** Potenciação
+    * % Resto da divisão
+    * Incremento = ++
+    * Decremento = --
+    * NaN = Not a number
+*/
+  let contador = 0
+  contador += 2
+  contador += 3
+  contador += 2
+  console.log(contador)
