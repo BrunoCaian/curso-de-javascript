476c4dd04adc88e12319680f1009b2da7cbbbd5d
BrunoCaian <brunocaian81@gmail.com>
2024-07-17 21:14:33 -0300

    exercicios

---
+++ Exercícios/aula22/index.js
@@ -0,0 +1,9 @@
+/*
+Operadores lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+//const expressaoAnd = true && true && false && true
+const expressaoOr = true || false
+console.log(expressaoOr)
