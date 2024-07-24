782faca919397f49daf5906dbd4b26f512818094
BrunoCaian <brunocaian81@gmail.com>
2024-07-23 22:58:01 -0300

    exercicios

--- Exercícios/aula32/index.js
+++ Exercícios/aula32/index.js
@@ -1,3 +1,10 @@
-const numeros = [1000, 2000, 3000, 40000, 500, 6000, 70, 800, 9000];
+/*const numeros = [1000, 2000, 3000, 40000, 500, 6000, 70, 800, 9000];
 const [um, , , quatro, , seis] = numeros;
 console.log(um, quatro, seis)
+*/
+let a = 'A'; // B
+let b = 'B'; // C
+let c = 'C'; // A
+
+[a, b, c] = [b, c, a];
+console.log(a, b, c)
---
+++ Exercícios/aula34/index.js
@@ -0,0 +1,5 @@
+const frutas = ['Banana', 'Abacaxi', 'Melancia', 'Maçã', 'Goiaba', 'Uva', 'Siriguela', 'Melão', 'Cajarana'] 
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`índice ${i}`, frutas[i])
+}
