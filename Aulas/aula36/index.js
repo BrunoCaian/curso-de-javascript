7b859cf61482965f36bed1caa9696b8f1a214333
BrunoCaian <brunocaian81@gmail.com>
2024-07-26 23:23:18 -0300

    aprendi sobre os for, while e do while

---
+++ Aulas/aula36/index.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome: 'Bruno',
+    sobrenome: 'Caian',
+    idade: 18,
+    sexo: 'Masculino',
+};
+
+    for (let i in pessoa) {
+        console.log(i, pessoa[i])
+    }
+
+/*for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+*/
+
+/*for (let i in frutas) {
+    console.log(frutas[i])
+}
+*/    
---
+++ Aulas/aula37/index.js
@@ -0,0 +1,35 @@
+const pessoa = {
+    nome: 'Bruno Caian',
+    idade: 18,
+    sexo: 'Masculino'
+}
+
+for (let i of pessoa) {
+    console.log(pessoa)
+}
+
+
+
+/*const frutas = ['Uva', 'Maçã', 'Pêra'];
+
+for (let i of frutas) {
+    console.log(i)
+}
+*/
+
+/*for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i])
+}
+*/
+
+/*for (let i in nome) {
+    console.log(nome[i])
+}
+*/    
+
+/*for (let i of nome) {
+    console.log(i)
+}
+*/
+
+
