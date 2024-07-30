52c12e8fdbfaa0b19f9669ccdf74e692910ab96d
BrunoCaian <brunocaian81@gmail.com>
2024-07-29 21:35:54 -0300

    atualizando

---
+++ Exercícios/ex03/index.js
@@ -0,0 +1,30 @@
+/* function soma(a, b) {
+    soma = a + b
+    return soma
+}
+
+console.log(soma(6, 10))
+*/
+
+/*function ePar(num) {
+    return num % 2 == 0 ? true : false
+}
+console.log(ePar(7))
+*/
+
+/*function fatorial(n) {
+    if (n < 0) {
+        return 0
+    } else if (n === 0) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(4))
+*/
+
+function ordenarArray(array) {
+
+}
