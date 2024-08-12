46d0df16249e02f09111c9494e560bcb6c74f07e
BrunoCaian <brunocaian81@gmail.com>
2024-08-11 22:25:34 -0300

    .

---
+++ Aulas/aula62/index.js
@@ -0,0 +1,10 @@
+// Valor por referência
+/*const nomes = ['Bruno', 'João', 'Pedro']
+nomes.push('Miguel')
+nomes.push(1)
+console.log(nomes)
+*/
+
+const nome = ['Bruno', 'Caian', 'Sombra']
+const nomes = nome.join(' ')
+console.log(nomes)
---
+++ Aulas/aula63/index.js
@@ -0,0 +1,4 @@
+const nomes = ['Bruno', 'Caian', 'Cauê', 'João']
+// nomes.splice(indice, delete, elem1, elem2, elem3 )
+const removidos = nomes.splice(2, 2, 'Luiz', 'Pedro')
+console.log(nomes, removidos)
