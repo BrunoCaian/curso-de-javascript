641070b6f7bebdce45ec27a13ec139a3aeae43b0
BrunoCaian <brunocaian81@gmail.com>
2024-08-15 21:32:51 -0300

    .

---
+++ Aulas/aula71/index.js
@@ -0,0 +1,33 @@
+// defineProperty - defineProperties
+
+function Produto(nome, preco, estoque) {
+    this.nome = nome
+    this.preco = preco
+    this.estoque = estoque
+
+    Object.defineProperty(this, 'estoque', {
+      enumerable: true, // mostra a chave
+      value: estoque,// valor
+      writable: true, // pode alterar
+      configurable: false // configurável  
+    })
+
+    Object.defineProperties(this, {
+     nome: {
+      enumerable: true, // mostra a chave
+      value: nome,// valor
+      writable: true, // pode alterar
+      configurable: false // configurável 
+     },
+     preco: {
+      enumerable: true, // mostra a chave
+      value: preco,// valor
+      writable: true, // pode alterar
+      configurable: false // configurável 
+     }   
+    })
+}
+
+const p1 = new Produto('Camisa', 20, 5)
+delete p1.estoque
+console.log(p1)
